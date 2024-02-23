58617dc197c13ab21282747443852210c6945b43
joyce0129 <131430612+joyce0129@users.noreply.github.com>
2024-02-24 00:10:05 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.font}>數位二甲111119026黃姿云</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  font:{
+    fontSize:40,
+  },
+
+});
